8639d0c1653c6e6cc792fc4836ccb9ebdde6693d
RahimliZakir <zakirer@code.edu.az>
2022-01-30 20:47:05 +0400

    From Component Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,7 @@ import { useCopyClipboard } from "../hooks";
 const Form = () => {
   const [data, setData] = useState("");
   const [url, setUrl] = useState([]);
+  const [error, setError] = useState("");
 
   const [isCopied, copyClipboard] = useCopyClipboard();
 
@@ -15,6 +16,8 @@ const Form = () => {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
+
+    if (data === "") setError("Please add alink");
 
     const {
       data: { result },
@@ -33,6 +36,7 @@ const Form = () => {
     ]);
 
     setData("");
+    setError("");
   };
 
   return (
@@ -40,31 +44,47 @@ const Form = () => {
       <Container>
         <Row className="justify-content-center">
           <Col md="8">
-            <form id="term-form" className="d-flex" onSubmit={handleFormSubmit}>
-              <input
-                className="form-control form-input"
-                placeholder="Shorthen a link here.."
-                value={data}
-                onChange={(e) => setData(e.target.value)}
-              />
-              <Button type="submit">Shorten it!</Button>
+            <form id="term-form" onSubmit={handleFormSubmit}>
+              <div className="d-flex">
+                <input
+                  className={`form-control form-input ${
+                    error === "" ? "" : "is-validation"
+                  }`}
+                  placeholder="Shorthen a link here.."
+                  value={data}
+                  onChange={(e) => setData(e.target.value)}
+                />
+                <Button type="submit">Shorten it!</Button>
+              </div>
+              {<p className="text-danger text-validation">{error}</p>}
             </form>
             <ListGroup>
               {url?.map((item, index) => {
                 return (
-                  <ListGroup.Item key={index + 1}>
-                    <span>Shorten:</span>
-                    <a href={item.short} target="_blank" rel="noreferrer">
-                      {item.short}
-                    </a>
-                    <span className="ms-2">Real: {item.real}</span>
-                    <Button
-                      variant={isCopied ? "primary" : "warning"}
-                      className="copy-btn"
-                      onClick={() => handleCopy(item.short)}
-                    >
-                      {isCopied ? "Copied" : "Copy"}
-                    </Button>
+                  <ListGroup.Item key={index + 1} className="my-3">
+                    <Row className="align-items-center">
+                      <Col lg="6" md="6" className="link-response-left">
+                        {item.real}
+                      </Col>
+                      <Col lg="6" md="6" className="link-response-right">
+                        <a
+                          href={item.short}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="me-3"
+                          style={{ color: "#2CD0CE" }}
+                        >
+                          {item.short}
+                        </a>
+                        <Button
+                          variant={isCopied ? "primary" : "warning"}
+                          className={`copy-btn ${isCopied ? "copied" : ""}`}
+                          onClick={() => handleCopy(item.short)}
+                        >
+                          {isCopied ? "Copied!" : "Copy"}
+                        </Button>
+                      </Col>
+                    </Row>
                   </ListGroup.Item>
                 );
               })}
